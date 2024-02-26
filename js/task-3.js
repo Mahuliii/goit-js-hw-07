f3648a90820d1b859a1f17184076269ebb51ebde
Mahuliii <148713111+Mahuliii@users.noreply.github.com>
2024-02-26 23:25:32 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+nameInput.addEventListener("input", () => {
+  const inputText = nameInput.value.trim();
+  if (inputText === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = inputText;
+  }
+});
